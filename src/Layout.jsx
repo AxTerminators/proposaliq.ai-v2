8c20d7b1334d7e90f0dcd83534cc1f6d8dd7bb3b
Abraham Xiong <abe@govcontractors.org>
2025-10-30 16:48:42 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,6 @@
-
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { createPageUrl, isConsultantAccount } from "@/utils";
+import { createPageUrl, isConsultantAccount } from "./utils";
 import {
   LayoutDashboard,
   FileText,
