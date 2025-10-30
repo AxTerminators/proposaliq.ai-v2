b013fa12ec7ba7d00d53fac9789422b8168c7761
Abraham Xiong <abe@govcontractors.org>
2025-10-30 03:52:47 +0000

    File changes

--- src/pages/AdminPortal.jsx
+++ src/pages/AdminPortal.jsx
@@ -1,7 +1,6 @@
-
 import React, { useState, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
-import PermissionChecker from "../components/admin/PermissionChecker";
+import { PermissionChecker } from "../components/admin/PermissionChecker";
 import { Card, CardContent } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
