402f9f2ea0b67b15cbcf774f7e6bb3906081e270
Abraham Xiong <abe@govcontractors.org>
2025-10-31 11:33:53 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,6 +1,5 @@
 import Dashboard from './pages/Dashboard';
 import Onboarding from './pages/Onboarding';
-import Proposals from './pages/Proposals';
 import ProposalBuilder from './pages/ProposalBuilder';
 import Resources from './pages/Resources';
 import Chat from './pages/Chat';
@@ -36,7 +35,6 @@ import Layout from './Layout.jsx';
 export const PAGES = {
     "Dashboard": Dashboard,
     "Onboarding": Onboarding,
-    "Proposals": Proposals,
     "ProposalBuilder": ProposalBuilder,
     "Resources": Resources,
     "Chat": Chat,
