38068b5e7885f892b3a0fe71795be21bd5843427
Abraham Xiong <abe@govcontractors.org>
2025-11-18 16:52:08 +0000

    File changes

--- src/components/dashboard/QuickActionsPanel.jsx
+++ src/components/dashboard/QuickActionsPanel.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { base44 } from "@/api/base44Client";
 import {
   Plus,
   LayoutGrid,
