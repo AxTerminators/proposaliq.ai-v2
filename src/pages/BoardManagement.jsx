c832f6444e284dbe251bef3014fd6b57b362a36f
Abraham Xiong <abe@govcontractors.org>
2025-11-18 14:32:52 +0000

    File changes

--- src/pages/BoardManagement.jsx
+++ src/pages/BoardManagement.jsx
@@ -17,7 +17,8 @@ import {
   CheckCircle2,
   Sparkles,
   Edit3,
-  Check
+  Check,
+  BarChart3
 } from "lucide-react";
 import {
   AlertDialog,
@@ -243,13 +244,22 @@ export default function BoardManagement() {
               Manage your Kanban boards and workflows
             </p>
           </div>
-          <Button
-            onClick={() => setShowCreateDialog(true)}
-            className="bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700"
-          >
-            <Plus className="w-5 h-5 mr-2" />
-            Create New Board
-          </Button>
+          <div className="flex gap-2">
+            <Button
+              onClick={() => window.location.href = '/TemplateAnalyticsDashboard'}
+              variant="outline"
+            >
+              <BarChart3 className="w-5 h-5 mr-2" />
+              Template Analytics
+            </Button>
+            <Button
+              onClick={() => setShowCreateDialog(true)}
+              className="bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700"
+            >
+              <Plus className="w-5 h-5 mr-2" />
+              Create New Board
+            </Button>
+          </div>
         </div>
 
         {/* Stats Cards */}
