796ae23e0515a609864aba6b4ba90f6f86fe28a3
Abraham Xiong <abe@govcontractors.org>
2025-11-23 02:49:49 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -3,11 +3,9 @@ import Onboarding from './pages/Onboarding';
 import ProposalBuilder from './pages/ProposalBuilder';
 import Resources from './pages/Resources';
 import Chat from './pages/Chat';
-import Subscription from './pages/Subscription';
 import Discussions from './pages/Discussions';
 import AdminPortal from './pages/AdminPortal';
 import LandingPage from './pages/LandingPage';
-import Home from './pages/Home';
 import Pricing from './pages/Pricing';
 import Settings from './pages/Settings';
 import Analytics from './pages/Analytics';
@@ -19,7 +17,6 @@ import Calendar from './pages/Calendar';
 import Tasks from './pages/Tasks';
 import TeamingPartners from './pages/TeamingPartners';
 import Feedback from './pages/Feedback';
-import RateFeedback from './pages/RateFeedback';
 import ClientPortal from './pages/ClientPortal';
 import ClientProposalView from './pages/ClientProposalView';
 import CostEstimator from './pages/CostEstimator';
@@ -30,41 +27,28 @@ import Tools from './pages/Tools';
 import Pipeline from './pages/Pipeline';
 import AnalyticsDashboard from './pages/AnalyticsDashboard';
 import ErrorMonitoringDashboard from './pages/ErrorMonitoringDashboard';
-import TemplatesLibrary from './pages/TemplatesLibrary';
 import AdvancedSearch from './pages/AdvancedSearch';
 import KeyPersonnel from './pages/KeyPersonnel';
 import WinLossCapture from './pages/WinLossCapture';
 import WinLossInsights from './pages/WinLossInsights';
 import BestPractices from './pages/BestPractices';
 import OnboardingGuide from './pages/OnboardingGuide';
-import AIFeatureRoadmap from './pages/AIFeatureRoadmap';
-import ClientPortalRoadmap from './pages/ClientPortalRoadmap';
-import SystemVerification from './pages/SystemVerification';
 import SystemDocumentation from './pages/SystemDocumentation';
 import ContentDevelopment from './pages/ContentDevelopment';
 import FinalReview from './pages/FinalReview';
-import KanbanTestingGuide from './pages/KanbanTestingGuide';
-import KanbanRoadmapStatus from './pages/KanbanRoadmapStatus';
 import PricingBuilder from './pages/PricingBuilder';
 import CompetitorIntel from './pages/CompetitorIntel';
-import TestJourney from './pages/TestJourney';
 import TemplateManager from './pages/TemplateManager';
 import BoardManagement from './pages/BoardManagement';
 import ContentLibrary from './pages/ContentLibrary';
-import AddTeamingPartner from './pages/AddTeamingPartner';
 import ClientOrganizationManager from './pages/ClientOrganizationManager';
 import ConsolidatedReporting from './pages/ConsolidatedReporting';
-import DataIsolationTest from './pages/DataIsolationTest';
 import ConsultantDashboard from './pages/ConsultantDashboard';
 import ClientDataCallPortal from './pages/ClientDataCallPortal';
 import DataCalls from './pages/DataCalls';
-import DataCallDocumentation from './pages/DataCallDocumentation';
-import RAGEnhancementStatus from './pages/RAGEnhancementStatus';
 import RAGPerformanceDashboard from './pages/RAGPerformanceDashboard';
 import AdminTemplateEditor from './pages/AdminTemplateEditor';
 import SystemBoardTemplateBuilder from './pages/SystemBoardTemplateBuilder';
-import DynamicModalDemo from './pages/DynamicModalDemo';
-import ModalFeatureStatus from './pages/ModalFeatureStatus';
 import ModalBuilder from './pages/ModalBuilder';
 import SuperAdminAiSettings from './pages/SuperAdminAiSettings';
 import AITokenUsageDashboard from './pages/AITokenUsageDashboard';
@@ -83,11 +67,9 @@ export const PAGES = {
     "ProposalBuilder": ProposalBuilder,
     "Resources": Resources,
     "Chat": Chat,
-    "Subscription": Subscription,
     "Discussions": Discussions,
     "AdminPortal": AdminPortal,
     "LandingPage": LandingPage,
-    "Home": Home,
     "Pricing": Pricing,
     "Settings": Settings,
     "Analytics": Analytics,
@@ -99,7 +81,6 @@ export const PAGES = {
     "Tasks": Tasks,
     "TeamingPartners": TeamingPartners,
     "Feedback": Feedback,
-    "RateFeedback": RateFeedback,
     "ClientPortal": ClientPortal,
     "ClientProposalView": ClientProposalView,
     "CostEstimator": CostEstimator,
@@ -110,41 +91,28 @@ export const PAGES = {
     "Pipeline": Pipeline,
     "AnalyticsDashboard": AnalyticsDashboard,
     "ErrorMonitoringDashboard": ErrorMonitoringDashboard,
-    "TemplatesLibrary": TemplatesLibrary,
     "AdvancedSearch": AdvancedSearch,
     "KeyPersonnel": KeyPersonnel,
     "WinLossCapture": WinLossCapture,
     "WinLossInsights": WinLossInsights,
     "BestPractices": BestPractices,
     "OnboardingGuide": OnboardingGuide,
-    "AIFeatureRoadmap": AIFeatureRoadmap,
-    "ClientPortalRoadmap": ClientPortalRoadmap,
-    "SystemVerification": SystemVerification,
     "SystemDocumentation": SystemDocumentation,
     "ContentDevelopment": ContentDevelopment,
     "FinalReview": FinalReview,
-    "KanbanTestingGuide": KanbanTestingGuide,
-    "KanbanRoadmapStatus": KanbanRoadmapStatus,
     "PricingBuilder": PricingBuilder,
     "CompetitorIntel": CompetitorIntel,
-    "TestJourney": TestJourney,
     "TemplateManager": TemplateManager,
     "BoardManagement": BoardManagement,
     "ContentLibrary": ContentLibrary,
-    "AddTeamingPartner": AddTeamingPartner,
     "ClientOrganizationManager": ClientOrganizationManager,
     "ConsolidatedReporting": ConsolidatedReporting,
-    "DataIsolationTest": DataIsolationTest,
     "ConsultantDashboard": ConsultantDashboard,
     "ClientDataCallPortal": ClientDataCallPortal,
     "DataCalls": DataCalls,
-    "DataCallDocumentation": DataCallDocumentation,
-    "RAGEnhancementStatus": RAGEnhancementStatus,
     "RAGPerformanceDashboard": RAGPerformanceDashboard,
     "AdminTemplateEditor": AdminTemplateEditor,
     "SystemBoardTemplateBuilder": SystemBoardTemplateBuilder,
-    "DynamicModalDemo": DynamicModalDemo,
-    "ModalFeatureStatus": ModalFeatureStatus,
     "ModalBuilder": ModalBuilder,
     "SuperAdminAiSettings": SuperAdminAiSettings,
     "AITokenUsageDashboard": AITokenUsageDashboard,
--- src/pages/ModalFeatureStatus.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import FeatureStatusDashboard from '../components/proposals/modals/FeatureStatusDashboard';
-
-export default function ModalFeatureStatus() {
-  return <FeatureStatusDashboard />;
-}
